ac83809a21d9eff7e7d9286f64801f376c009f39
David Loza <davicholoza1990@gmail.com>
2021-10-21 19:06:57 -0500

    Mi cuarto commit

--- operacion.js
+++ operacion.js
@@ -6,3 +6,26 @@ function suma(){
 
     console.log(suma);
 }
+
+
+class persona{
+    alto; ancho;
+    constructor(alto,ancho){
+        this.alto= alto;
+        this.ancho= ancho;
+    }
+
+    Area(){
+        var area = this.alto * this.ancho;
+        console.log(area);
+        alert(area);
+    }
+}
+
+function ress(){
+    var rec=new persona(12,4);
+
+    rec.Area();
+}
+
+
